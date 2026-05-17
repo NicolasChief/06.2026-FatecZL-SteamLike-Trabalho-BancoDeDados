1fe11f82fd7a2fa5303c0eb87d70858276a72d38
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2026-05-17 11:17:03 -0300

    Enhance Genero class with constructors and methods
    Added constructors and getter/setter methods for generoJogo.

--- app/src/main/java/edu/curso/model/Genero.java
+++ app/src/main/java/edu/curso/model/Genero.java
@@ -4,4 +4,18 @@ public class Genero {
 
     private String generoJogo;
 
+    public Genero() {
+    }
+
+    public Genero(String generoJogo) {
+        this.generoJogo = generoJogo;
+    }
+
+    public String getGeneroJogo() {
+        return generoJogo;
+    }
+    public void setGeneroJogo(String generoJogo) {
+        this.generoJogo = generoJogo;
+    }
+
 }
