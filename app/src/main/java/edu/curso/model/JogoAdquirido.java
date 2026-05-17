df1c3a44a3232d23d63c85adae42890c5576bcce
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2026-05-17 11:18:38 -0300

    Add constructors and improve rodar method

--- app/src/main/java/edu/curso/model/JogoAdquirido.java
+++ app/src/main/java/edu/curso/model/JogoAdquirido.java
@@ -7,12 +7,43 @@ public class JogoAdquirido {
     private Date dataAquisicao;
     private Jogo jogo; 
 
-    public void rodar() {
+    public JogoAdquirido() {
+    }
 
+    public JogoAdquirido(Date dataAquisicao, Jogo jogo) {
+        this.dataAquisicao = dataAquisicao;
+        this.jogo = jogo;
+    }
+
+    public Date getDataAquisicao() {
+        return dataAquisicao;
+    }
+    public void setDataAquisicao(Date dataAquisicao) {
+        this.dataAquisicao = dataAquisicao;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public void rodar() {
+        if (jogo != null) {
+            System.out.println("Rodando: " + jogo.getNome());
+        } else {
+            System.out.println("Nenhum jogo disponível para rodar.");
+        }
     }
 
     public void desinstalarJogo() {
-        
+        if (jogo != null) {
+            System.out.println("Desinstalando: " + jogo.getNome());
+            jogo = null;
+        } else {
+            System.out.println("Nenhum jogo instalado para desinstalar.");
+        }
     }
 
 }
