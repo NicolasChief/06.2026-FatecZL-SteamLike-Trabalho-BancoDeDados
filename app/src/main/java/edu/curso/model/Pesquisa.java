6a18b0ed7657238aadebc8c52383b81ff77d46bb
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2026-05-17 11:20:04 -0300

    Enhance Pesquisa class with constructors and methods
    Added constructors, getter, setter, and method to add games.

--- app/src/main/java/edu/curso/model/Pesquisa.java
+++ app/src/main/java/edu/curso/model/Pesquisa.java
@@ -7,8 +7,34 @@ public class Pesquisa {
 
     private List<Jogo> campoBusca = new ArrayList<>();
 
+    public Pesquisa() {
+    }
+
+    public Pesquisa(List<Jogo> campoBusca) {
+        this.campoBusca = campoBusca;
+    }
+
+    public List<Jogo> getCampoBusca() {
+        return campoBusca;
+    }
+    public void setCampoBusca(List<Jogo> campoBusca) {
+        this.campoBusca = campoBusca;
+    }
+
+    public void adicionarJogo(Jogo jogo) {
+        this.campoBusca.add(jogo);
+    }
+
     public void exibirPesquisa() {
-        
+        if (campoBusca.isEmpty()) {
+            System.out.println("Nenhum resultado encontrado.");
+            return;
+        }
+
+        for (Jogo jogo : campoBusca) {
+            jogo.exibirDetalhes();
+            System.out.println("---");
+        }
     }
 
 }
