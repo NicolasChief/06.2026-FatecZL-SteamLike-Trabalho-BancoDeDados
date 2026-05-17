5e35dea317388aa66fcc9163c18bc841df18110d
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2026-05-17 11:17:29 -0300

    Add constructors and methods to ItemPedido class

--- app/src/main/java/edu/curso/model/ItemPedido.java
+++ app/src/main/java/edu/curso/model/ItemPedido.java
@@ -5,4 +5,30 @@ public class ItemPedido {
     private int quantidade;
     private double precoUni;
 
+    public ItemPedido() {
+    }
+
+    public ItemPedido(int quantidade, double precoUni) {
+        this.quantidade = quantidade;
+        this.precoUni = precoUni;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUni() {
+        return precoUni;
+    }
+    public void setPrecoUni(double precoUni) {
+        this.precoUni = precoUni;
+    }
+
+    public double calcularTotal() {
+        return quantidade * precoUni;
+    }
+
 }
