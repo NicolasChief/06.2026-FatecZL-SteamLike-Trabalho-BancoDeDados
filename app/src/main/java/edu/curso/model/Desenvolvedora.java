c77b224ff339a671258f7b4acf8895bbd6716ad2
Nicolas Boffo <106088482+NicolasChief@users.noreply.github.com>
2026-05-17 11:15:21 -0300

    Add constructors and portfolio management methods

--- app/src/main/java/edu/curso/model/Desenvolvedora.java
+++ app/src/main/java/edu/curso/model/Desenvolvedora.java
@@ -7,4 +7,23 @@ public class Desenvolvedora extends Estudio {
 
     private List<Jogo> portifolio = new ArrayList<>();
 
+    public Desenvolvedora() {
+        super();
+    }
+
+    public Desenvolvedora(String nome, String cnpjcpf) {
+        super(nome, cnpjcpf);
+    }
+
+    public List<Jogo> getPortifolio() {
+        return portifolio;
+    }
+    public void setPortifolio(List<Jogo> portifolio) {
+        this.portifolio = portifolio;
+    }
+
+    public void adicionarJogo(Jogo jogo) {
+        this.portifolio.add(jogo);
+    }
+
 }
